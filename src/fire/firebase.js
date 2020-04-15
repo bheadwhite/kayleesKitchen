258f28b59b2c0e8a75e5509c3f9a2499110f354b
Brent Whitehead <bheadwhite@gmail.com>
2020-04-15 01:46:33 -0600

    added analytics

--- src/fire/firebase.js
+++ src/fire/firebase.js
@@ -13,6 +13,7 @@ const config = {
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 }
 firebase.initializeApp(config)
+firebase.analytics()
 
 const firestoreRef = firebase.firestore()
 export const authRef = firebase.auth()
